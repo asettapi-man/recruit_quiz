dd180baf3ea88cfdd0565aea6373ab481ce109c9
asepi <shuya.t615@gmail.com>
2026-05-26 12:16:49 +0900

    慣用句の問題作成

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -100,8 +100,8 @@ int main()
 	cin >> subject;
 	if (subject == 2) {
 		questions = CreateKanjiExam();
-		//QuestionList idioExam = CreateIdiomExam();
-		//questions.insert(questions.end(), idioExam.begin(), idioExam.end());
+		QuestionList idioExam = CreateIdiomExam();
+		questions.insert(questions.end(), idioExam.begin(), idioExam.end());
 		QuestionList homophoneExam = CreateHomophoneExam();
 		questions.insert(questions.end(), homophoneExam.begin(), homophoneExam.end());
 		QuestionList antonyExam = CreateAntonyExam();
