d0f11907e3320d9bb65673850dc58022b1cdb465
asepi <shuya.t615@gmail.com>
2026-08-25 10:46:42 +0900

    数学の問題を別のファイルで管理した

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -5,105 +5,26 @@
 #include "exam_science.h"
 #include "exam_geography.h"
 #include "exam_politics.h"
+#include "exam_mathematics.h"
 #include <iostream>
 #include <string>
 #include <vector>
 #include <random>
 using namespace std;
 
-/// <summary>
-/// 最大公約数を求める
-/// </summary>
-/// <returns></returns>
-int gcd(int a, int b)
-{
-	//余りが０になった時の除数を返す
-	while (b)
-	{
-		int r = a % b;
-		a = b;	//除数を次の被除数にする
-		b = r;	//余りを次の除数にする
-	}
-	return a;
-}
-
 int main()
 {
 	vector<Question> questions(3);
-
-	random_device rd;
-	mt19937 rand(rd());
-
-	//掛け算
-	int x = uniform_int_distribution<>(1, 30)(rand);
-	int y = uniform_int_distribution<>(1, 20)(rand);
-	questions[0].q = to_string(x) + "x" + to_string(y) + "の答えは？";
-	questions[0].a = to_string(x * y);
-
-	//割り算
-	x = uniform_int_distribution<>(1, 30)(rand);
-	y = uniform_int_distribution<>(1, 20)(rand);
-	questions[1].q = to_string(x * y) + "/" + to_string(y) + "の答えは？";
-	questions[1].a = to_string(x);
-
-	//複雑な式
-	x = uniform_int_distribution<>(1, 100)(rand);
-	y = uniform_int_distribution<>(1, 10)(rand);
-	int z = uniform_int_distribution<>(1, 10)(rand);
-	int w = uniform_int_distribution<>(1, 10)(rand);
-	questions[2].q = to_string(x) + "-(" + to_string(y * w) + "+" + to_string(z * w) + ")/" + to_string(w) + "の答えは？";
-	questions[2].a = to_string(x - (y - z));
-
-	//三角形の面積
-	x = uniform_int_distribution<>(1, 10)(rand);
-	y = uniform_int_distribution<>(1, 5)(rand) * 2;
-	questions.push_back({ "面積" + to_string(x * y / 2) + "cm^2、底辺" + to_string(y) + "cmの三角形の高さを求めよ。", to_string(x) });
-
-	//円錐の体積
-	x = uniform_int_distribution<>(1, 10)(rand);
-	y = uniform_int_distribution<>(1, 5)(rand) * 3;
-	questions.push_back({ "底面の半径" + to_string(x) + "cm、高さ" + to_string(y) + "cmの円錐がある。\n" + "この円錐の体積をXπcm^3とする。Xの値を求めよ。", to_string(x * x * y / 3) });
-
-	//球の体積
-	x = uniform_int_distribution<>(1, 5)(rand) * 3;
-	questions.push_back({ "半径" + to_string(x) + "cmの球がある。\n" + "この球の体積をXπcm^3とする。Xの値を求めよ。", to_string(4 * x * x * x / 3) });
-
-	//サイコロの確率
-	x = uniform_int_distribution<>(1, 5)(rand);
-	y = uniform_int_distribution<>(1, 6 - x)(rand);
-	z = gcd(y + 1, 6);
-	questions.push_back({ "サイコロを１個振って、" + to_string(x) + "から" + to_string(x + y) + "が出る確率を求めよ。", to_string((y + 1) / z) + "/" + to_string(6 / z) });
-
-	//順列
-	x = uniform_int_distribution<>(3, 7)(rand);
-	y = uniform_int_distribution<>(1, x)(rand);
-	z = 1;
-	for (int i = 0; i < y; i++)
-	{
-		z *= x - i;
-	}
-	questions.push_back({ to_string(x) + "人のうち" + to_string(y) + "人を選んで並べる方法は何通りあるか？", to_string(z) });
-
-	//組み合わせ
-	x = uniform_int_distribution<>(3, 6)(rand);
-	y = uniform_int_distribution<>(1, x)(rand);
-	z = 1;
-	for (int i = 0; i < y; i++)
-	{
-		z *= x - i;	//分子の計算
-	}
-	for (int i = 0; i < y; i++)
-	{
-		z /= y - i;	//分母の計算
-	}
-	questions.push_back({ to_string(x) + "人のうち" + to_string(y) + "人を選ぶ組み合わせは何通りあるか？", to_string(z) });
 
 	cout << "[リクルート試験対策クイズ]\n";
 
 	cout << "今日かを選んでください\n１=数学\n２=国語\n３=英語\n４=理科\n５=地理\n６=政治\n";
 	int subject;
 	cin >> subject;
-	if (subject == 2) {
+	if (subject == 1) {
+		questions = CreateMathematicsExam();
+	}
+	else if (subject == 2) {
 		questions = CreateKanjiExam();
 		QuestionList idioExam = CreateIdiomExam();
 		questions.insert(questions.end(), idioExam.begin(), idioExam.end());
